218236453d39dcc6dae548b60ce58d06d223e724
Sunki Baek <sunki.baek@gmail.com>
2018-05-02 16:00:10 +0900

    Fix import syntax

--- ios/RCTWeiboAPI/RCTWeiboAPI.h
+++ ios/RCTWeiboAPI/RCTWeiboAPI.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 erica. All rights reserved.
 //
 
-if __has_include(<React/RCTBridgeModule.h>)
+#if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
 #else
 #import "RCTBridgeModule.h"
